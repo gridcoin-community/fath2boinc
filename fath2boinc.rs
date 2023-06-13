d842bed22db13d01cf5da34802936e4b89cebcb4
div72 <noreply@div72.xyz>
2023-06-13 19:21:06 +0300

    fath2boinc-rs: add stat update func

--- fath2boinc.rs
+++ fath2boinc.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fmt;
-use std::fmt::Write;
+
+const LN2: f64 = 0.693147180559945309417;
+const CREDIT_HALF_LIFE: f64 = 86400.0 * 7.0;
 
 fn is_md5_hex(data: String) -> bool {
     return (data.len() == 32) && data.chars().all(|c| -> bool { c.is_ascii_hexdigit() });
@@ -27,6 +28,31 @@ impl User {
 
     fn to_csv(&self) -> String {
         return format!("{0:.8},{1:.8},{2:.8},{3}\n", self.total_credit, self.expavg_credit, self.expavg_time, self.cpid);
+    }
+
+    fn update_stats(&mut self, new_total_credit: f64, now: f64) {
+        // Based on BOINC RAC calculations.
+        // See https://github.com/BOINC/boinc/blob/73a7754e7fd1ae3b7bf337e8dd42a7a0b42cf3d2/html/inc/credit.inc#L24
+        let work = new_total_credit - self.total_credit;
+        if work < 0.0 {
+            return;
+        }
+        self.total_credit = new_total_credit;
+
+        if self.expavg_time > 0.0 {
+            let diff = f64::max(now - self.expavg_time, 0.0);
+            let diff_days = diff / 86400.0;
+            let weight = (-diff * LN2 / CREDIT_HALF_LIFE).exp();
+
+            self.expavg_credit *= weight;
+            if (1.0 - weight) > 0.000001 {
+                self.expavg_credit += (1.0 - weight) * (work / diff_days);
+            } else {
+                self.expavg_credit += LN2 * work * 86400.0 / CREDIT_HALF_LIFE;
+            }
+        }
+
+        self.expavg_time = now;
     }
 }
 
