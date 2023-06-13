ec7b85a8903245d0dd98eadea93f6cd1a7fd8e7c
div72 <noreply@div72.xyz>
2023-06-13 10:55:56 +0300

    fath2boinc-rs: add is_md5_hex helper

--- fath2boinc.rs
+++ fath2boinc.rs
@@ -1,5 +1,9 @@
 use std::env;
 
+fn is_md5_hex(data: String) -> bool {
+    return (data.len() == 32) && data.chars().all(|c| -> bool { c.is_ascii_hexdigit() });
+}
+
 fn main() -> Result<(), String> {
     if env::args().len() != 4 {
         return Err("USAGE: fath2boinc <local data path> <f@h data path> <boinc data path>".to_string());
